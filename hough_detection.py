d577b7ea7a4b9d5ef0bc63d3ea6366f486d2fcf4
joeydash <joydassudipta@gmail.com>
2017-12-19 15:58:52 +0530

    fix:done

--- hough_detection.py
+++ hough_detection.py
@@ -9,16 +9,17 @@ while 1:
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
     if lines is not None:
-        for r, theta in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * r
-            y0 = b * r
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * a)
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * a)
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        for i in range(len(lines)):
+            for r, theta in lines[i]:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * r
+                y0 = b * r
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * a)
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * a)
+                cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.imshow('img', frame)
     k = cv2.waitKey(1)
     if k == 27:
