8a52cfb5f28fcd1364d751311943f5fd22f0aab6
joeydash <joydassudipta@gmail.com>
2017-12-19 15:56:11 +0530

    fix:done

--- hough_detection.py
+++ hough_detection.py
@@ -1,27 +1,29 @@
 #!/usr/bin/ python 2.7.12
 import cv2
 import numpy as np
-cap=cv2.VideoCapture(0)
-while(1):
-	ret, frame=cap.read()
-	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	edges=cv2.Canny(gray,50,150,apertureSize=3)
-	lines=cv2.HoughLines(edges,1,np.pi/180,200)
-	if lines[0].all != None :		
-		for r,theta in lines[0]:
-			a=np.cos(theta)
-			b=np.sin(theta)
-			x0=a*r
-			y0=b*r
-			x1=int(x0+1000*(-b))
-			y1=int(y0+1000*(a))
-			x2=int(x0-1000*(-b))
-			y2=int(y0-1000*(a))
-			cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
-		#cv2.imwrite('hough_lines.jpg',img)
-		cv2.imshow('img',frame)
-	
-	k= cv2.waitKey(5) & 0xFF
-	if (k==27): break
+
+cap = cv2.VideoCapture(0)
+while 1:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+    if lines is not None:
+        for r, theta in lines[0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * r
+            y0 = b * r
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * a)
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * a)
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.imshow('img', frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+    else:
+        pass
 cap.release()
 cv2.destroyAllWindows()
--- joeyJunks/cameraCheck.py
+++ joeyJunks/cameraCheck.py
@@ -5,15 +5,7 @@ template1 = cv2.imread('haz.png')
 template = cv2.cvtColor(template1, cv2.COLOR_BGR2GRAY)
 while True:
     ret, frame = cap.read()
-    # img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # w, h = template.shape[::-1]
-    # res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    # threshold = 0.6
-    # loc = np.where(res >= threshold)
-    # for pt in zip(*loc[::-1]):
-    #     cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
-
-    cv2.imshow('Detected', template)
+    cv2.imshow('Detected', frame)
 
     k = cv2.waitKey(1)
     if k == 27:
@@ -36,4 +28,4 @@ while True:
         print k
         # pass
 cap.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
