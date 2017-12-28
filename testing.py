c155ce8b6b400cc5ae400fb2f1aec20ab7a02354
atharvasaraf <atharvasaraf@yahoo.in>
2017-12-28 20:01:31 +0530

    testing changes

--- testing.py
+++ testing.py
@@ -1,27 +1,20 @@
-#!/usr/bin/ python 2.7.12
+#!/usr/b`in/ python 2.7.12
 import cv2
 import numpy as np
 
 #-------------USING WEBCAM----------------------
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 #-----------------------------------------------
-
 
 #------------USING RPICAM----------------------
 
 
 #-----------------------------------------------
 
-
-
-#-----masking parameters for filtering Yellow Colour-------
+#-----Masking parameters for filtering Yellow Colour-------
 lower_color = np.array([20, 45, 80])
 upper_color = np.array([40, 130, 220])
 #-----------------------------------------------------
-
-
-
-
 
 while 1:
 	sum_r=0
@@ -31,30 +24,16 @@ while 1:
 	blur = cv2.GaussianBlur(hsv, (15, 15), 0)
 	mask = cv2.inRange(hsv, lower_color, upper_color)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	
-	# ----------------------------
-	# sobel experimental:
-	# gradX = cv2.Sobel(gray, ddepth=cv2.CV_32F,dx=1,dy=0,ksize=-1)
-	# gradY = cv2.Sobel(gray, ddepth=cv2.CV_32F,dx=0,dy=1,ksize=-1)
-	# gradient=cv2.subtract(gradX,gradY)
-	# edges=cv2.convertScaleAbs(gradient)
-	# ----------------------------
-	# ----------------------------
-	# canny is original
 	edges = cv2.Canny(mask, 35, 230, apertureSize=3)
-	# ----------------------------
-
 	lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
 	if lines is not None:
-		#sum_r=0
-		#sum_theta=0
-		for i in range(len(lines)):
+		if len(lines)>4:
+			check=4
+		elif len(lines)<4:
+			check=len(lines)
+		
+		for i in range(check):
 			for r, theta in lines[i]:
-				if theta>1.57079:
-					theta=-3.14159+theta
-				sum_theta=sum_theta + theta
-				sum_r=sum_r+r
-
 				a = np.cos(theta)
 				b = np.sin(theta)
 				x0 = a * r
@@ -64,29 +43,32 @@ while 1:
 				x2 = int(x0 - 1000 * (-b))
 				y2 = int(y0 - 1000 * (a))
 				cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-				#print "r : ",r,"theta : ",theta       			
-				#cv2.imwrite('hough_lines.jpg',img)
-			sum_theta=sum_theta/len(lines)
-			sum_r=sum_r/len(lines)
-			print "sumtheta: ",sum_theta," sumr: ",sum_r, " len: ",len(lines[i]), "	theta",theta,"	r ",r
-			a=np.cos(sum_theta)
-			b=np.sin(sum_theta)
-			x0=a*sum_r
-			y0=b*sum_r
-			x1=int(x0 + 1000 * (-b))
-			y1=int(y0 + 1000 * (a))
-			x2=	int(x0 - 1000 * (-b))
-			y2=int(y0 - 1000 * (a))
-			cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),5)
+			
+			if theta>-1.57079 and theta<=-0.78539:
+				theta=2.35619+theta
+			elif theta>-0.78539 and theta<0:
+				theta=theta+1.57079
+			sum_theta=sum_theta+theta
+			sum_r=sum_r+r
+		
+		sum_theta=sum_theta/check
+		sum_r=sum_r/check
+		a=np.cos(sum_theta)
+		b=np.sin(sum_theta)
+		x0=a*sum_r
+		y0=b*sum_r
+		x1=int(x0 + 1000 * (-b))
+		y1=int(y0 + 1000 * (a))
+		x2=	int(x0 - 1000 * (-b))
+		y2=int(y0 - 1000 * (a))
+		cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),5)
+	
 	sum_r=0
 	sum_theta=0
 	cv2.imshow('img', frame)
-	
-	#print(sum_theta*57.281)
-	#print(sum_r)
-	#cv2.imshow('edges', edges)
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
 		break
+
 cap.release()
 cv2.destroyAllWindows()
