b1820661094343a142febbd70dc53b90f5084cef
atharvasaraf <atharvasaraf@yahoo.in>
2018-01-01 18:06:44 +0530

    Touchup required

--- testing_again.py
+++ testing_again.py
@@ -33,23 +33,6 @@ def findPoints(r,theta,length):
 #---------------------------------------------------------------	
 
 
-#---------------getPoint function-------------------------------
-#----Return Intersection of 2 polar lines in cartesian----------
-def getPoint(r1,theta1,r2,theta2):
-	ct1=math.cos(theta1)
-	st1=math.sin(theta1)
-	ct2=math.cos(theta2)
-	st2=math.sin(theta2)
-	d=ct1*st1-st1*ct2
-	if d != 0.0:
-		x=int((st2*r1 - st1*r2)/d)
-		y=int((-ct2*r1 + ct1*r2)/d)
-		return (x,y)
-	else:
-		return None
-#---------------------------------------------------------------
-
-
 while 1:
 	#setting all working variables to default after every loop-----
 	sum_theta_secondary=0
@@ -59,6 +42,7 @@ while 1:
 	count_secondary=0
 	count_main=0
 	a=[]
+	
 	#capture a frame from the video feed
 	ret, frame = cap.read()
 	
@@ -72,6 +56,7 @@ while 1:
 	#---------------Line Detection Using HoughLines function-------
 	lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
 	
+	
 	#---------------Execute while lines are found------------------
 	if lines is not None:
 		
@@ -81,24 +66,28 @@ while 1:
 		elif len(lines)>4:
 			check=4
 		
-		#--------------Loop through each line ------------------------
+		#--------------Looping through each line ------------------------
 		for i in range(check):
 			for r, theta in lines[i]:			
+				#making an array of the r found for boundary conditions-----
 				a=np.append(a,r)
+				
 				#--Call findPoints function to obtain cartesian of line--
 				points=findPoints(r,theta,1000)
+				
+				#-----Transform found lines appropriately for averaging----- 
 				if r < 0:
 					theta=theta-math.radians(180)	
-				#-----Identify angle difference between current line and longest line--
+				
+				#-----Identify angle difference between current line and Vertical--
 				theta_diff=np.degrees(theta)
 				
+				#------Classify line as Primary or Secondary based on the Angle perceived
 				if theta_diff>45 and theta_diff<135:
-					#print "Detecting secondary line at ",theta_diff
 					sum_theta_secondary=sum_theta_secondary+theta
 					sum_r_secondary=sum_r_secondary+abs(r)
 					count_secondary=count_secondary+1
 				else :
-					#print "Detecting primary line at ",theta_diff 
 					sum_theta_main=sum_theta_main+theta
 					sum_r_main=sum_r_main+abs(r)
 					count_main=count_main+1
@@ -106,12 +95,8 @@ while 1:
 			    	
 				#----------------Draw found line in Green---------------
 				#cv2.line(frame, points[0], points[1], (0, 255, 0), 2)
-				
-				#----------Adjust angle for -ve r----------------------
-			
-				
+												
 				#-----------Compute Sum for averaging -----------
-		#print ""
 		a=np.sign(a)
 		a=np.sum(a)
 		if count_main is not 0:
@@ -121,17 +106,23 @@ while 1:
 			sum_theta_secondary=sum_theta_secondary/count_secondary
 			sum_r_secondary=sum_r_secondary/count_secondary
 	 	
+		#-------Correction for boundary conditions--------------
 		if abs(a)>=4:
 			points=findPoints(sum_r_main,sum_theta_main,1000)
 		else :
 			points=findPoints(lines[0][0][0],lines[0][0][1],1000)
 		
+		#-------Draw Primary and Secondary lines after averaging and applying boundary condition
 		cv2.line(frame,points[0],points[1],(0,0,255),2)
 		points=findPoints(sum_r_secondary,sum_theta_secondary,30)
+		print (points[0][0]+points[1][0])/2,"		",(points[0][1]+points[1][1])/2,"	coordinates of the center seondary line"
 		cv2.line(frame,points[0],points[1],(255,0,0),2)
+	
+	#--------------Check User Command for Loop termination------
 	k = cv2.waitKey(1) & 0xFF
 	if k == 27:
 		break
+	#---------------Show Image where lines have been identified-------------------------------------
 	cv2.imshow('img',frame)
 
 cap.release()
